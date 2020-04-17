2a8bfe376db9f342536f787a2b4aa984fd45799c
zpavloudis <astari@DESKTOP-AOP18KB.localdomain>
2020-04-17 13:18:14 +0100

    API endpoints now usable

--- IntoWebAPI/values.py
+++ IntoWebAPI/values.py
@@ -1,4 +1,5 @@
 from IntoWebAPI import app
+from flask import url_for
 from IntoWebAPI.records import query_db
 from datetime import date
 
@@ -7,28 +8,35 @@ def values():
     values = query_db('select * from records')
     return values
 
-@app.route('/values/<country>')
+@app.route('/values/country/<country>')
 def get_country_cases(country):
     values = query_db('select * from records where countriesAndTerritories = ?',
                         [country])
     return values
 
-@app.route('/values/<country_code>')
+@app.route('/values/country_code/<country_code>')
 def get_country_cases_code(country_code):
     values = query_db('select * from records where countryterritoryCode = ?',
                         [country_code])
+    print(country_code)
     return values
 
-@app.route('/values/date')
+@app.route('/values/date/today')
 def get_todays_cases():
     values = query_db('select * from records where dateRep = ?', 
-                        [date.today().strftime("%d/%m/%y")])
+                        [date.today().strftime("%d/%m/%Y")])
     return values
 
 @app.route('/values/date/<date>')
 def get_cases_for_date(date):
-   # dt = date[:2] + '/' + date[2:4] + '/' + date[4:6]
-   # print(dt)
     values = query_db('select * from records where dateRep = ?',
-        [date[:2] + '/' + date[2:4] + '/' + date[4:6]])
+        [date[:2] + '/' + date[2:4] + '/' + date[4:8]])
+    print('select * from records where dateRep = ?',date[:2] + '/' + date[2:4] + '/' + date[4:8])
     return values
+
+with app.test_request_context():
+    print(url_for('values'))
+    print(url_for('get_country_cases',country='Greece'))
+    print(url_for('get_country_cases_code',country_code='GBR'))
+    print(url_for('get_todays_cases'))
+    print(url_for('get_cases_for_date',date='08042020'))
